24544db912f2ab5a93f49e425a8b854319b5ee7a
emekeo <gistwithcollins@gmail.com>
2023-05-03 23:10:06 -0700

    async fixed

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,3 @@
-
- 
-
 #!/usr/bin/env python3
 
 ''' Desription: Import async_comprehension from the previous file and write a
@@ -11,18 +8,18 @@
                 it to yourself.
 '''
 
-from asyncio import gather
-from time import time
+import time
+import asyncio
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    ''' Measure the runtime of async_comprehension executed 4 times in
-        parallel. '''
-    first_time = time()
-    await gather(async_comprehension(), async_comprehension(),
-                 async_comprehension(), async_comprehension())
-    next_time = time()
-
-    return next_time - first_time
+    """
+    Collect 10 random numbers using an async comprehensing,
+    then return the 10 random numbers.
+    """
+    start: float = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end: float = time.perf_counter()
+    return (end - start)
